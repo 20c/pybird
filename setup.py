a95ecfd19dfde16f482ad93a8e5a2cccf793aba6
Erik Romijn (iMac) <erik@erik.io>
2011-06-20 21:54:22 +0200

    update version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pybird',
-      version='1.0.0',
+      version='1.0.1',
       description='BIRD interface handler for Python',
       author='Erik Romijn',
       author_email='eromijn@solidlinks.nl',
