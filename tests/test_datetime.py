824747dc37eb9fd742f660213ed33c43ac22aa63
Matt Griswold <grizz@20c.com>
2017-01-16 00:26:10 +0000

    fix time test

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,12 +1,6 @@
 
 import datetime
-from pybird import PyBird
 import pytest
-
-
-@pytest.fixture
-def bird():
-    return PyBird(None)
 
 
 def test_datetime(bird):
@@ -20,21 +14,33 @@ def test_date(bird):
 
 
 def test_time(bird):
-    now = datetime.datetime.now()
+    now = datetime.datetime(2019, 12, 10, 10, 12, 19)
 
-    expected = datetime.datetime.combine(now, datetime.time(14, 20))
-    assert expected == bird._calculate_datetime('14:20')
+    # before
+    expected = datetime.datetime(now.year, now.month, now.day, 4, 20)
+    assert expected == bird._calculate_datetime('4:20', now=now)
+
+    # same
+    expected = datetime.datetime.combine(now, datetime.time(10, 12))
+    assert expected == bird._calculate_datetime('10:12', now=now)
+
+    # after
+    expected = datetime.datetime(now.year, now.month, now.day - 1, 14, 20)
+    assert expected == bird._calculate_datetime('14:20', now=now)
 
 
 def test_monthday(bird):
     now = datetime.datetime(2019, 12, 10)
 
+    # before
     expected = datetime.datetime(2019, 6, 29)
     assert expected == bird._calculate_datetime('Jun29', now=now)
 
+    # same
     expected = datetime.datetime(2018, 12, 10)
     assert expected == bird._calculate_datetime('Dec10', now=now)
 
+    # after
     expected = datetime.datetime(2018, 12, 31)
     assert expected == bird._calculate_datetime('Dec31', now=now)
 
